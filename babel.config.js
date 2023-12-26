64490435574f73ee7e7ba07652cd82aece19dc35
Mario Mecaroni <mmecaroni@gmail.com>
2023-12-25 17:18:15 -0700

    Removed Typescript and file extensions from tsx to jsx

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "expo-router/babel",
-      "nativewind/babel",
-      "react-native-reanimated/plugin",
+      // Required for expo-router
+      'expo-router/babel',
     ],
   };
 };
